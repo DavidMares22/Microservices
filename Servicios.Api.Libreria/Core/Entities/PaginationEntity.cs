43734153537e41998f536984e66bf8fd2a0a0eb6
David Mares <damr18@gmail.com>
2025-07-03 07:23:53 -0600

    Libro Controller and Entity fixed namespace

--- Servicios.Api.Libreria/Core/Entities/PaginationEntity.cs
+++ Servicios.Api.Libreria/Core/Entities/PaginationEntity.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Servicios.api.Libreria.Core.Entities
+namespace Servicios.Api.Libreria.Core.Entities
 {
     public class PaginationEntity<TDocument>
     {
