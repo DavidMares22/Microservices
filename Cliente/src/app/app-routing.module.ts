08e8b9be4f1a972fe3d2fd3d37100d01072e0f78
David Mares <damr18@gmail.com>
2025-07-18 20:56:29 -0600

    books are displayed and clickable

--- Cliente/src/app/app-routing.module.ts
+++ Cliente/src/app/app-routing.module.ts
@@ -5,12 +5,16 @@ import { Routes, RouterModule } from '@angular/router';
  
 import {AutoresComponent} from './autores/autores.component';
 import { InicioComponent } from './inicio.component';
+import { LibrosComponent } from './libros/libros.component';
 
 
 const routes: Routes = [
   { path: '', component: InicioComponent }  ,
 
   { path: 'autores', component: AutoresComponent },
+  { path: 'libros', component: LibrosComponent },
+
+  
 ];
 
 @NgModule({
--- Cliente/src/app/libros/libros.component.ts
+++ Cliente/src/app/libros/libros.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrl: './libros.component.css'
 })
 export class LibrosComponent {
+ 
+  libros: string[] = ['Libro 1', 'Libro 2', 'Libro 3'];
+
+  eliminarLibro(libro: string) {
+    this.libros = this.libros.filter(l => l !== libro);
+    console.log('Libro eliminado:', libro);
+  }
 
 }
