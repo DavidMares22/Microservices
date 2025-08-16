56c028e1de79cb04d0a2d03c963c5babcaba0a26
David Mares <damr18@gmail.com>
2025-08-16 07:52:27 -0600

    Submit method created with validation

--- Cliente/src/app/seguridad/registrar/registrar.component.ts
+++ Cliente/src/app/seguridad/registrar/registrar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-registrar',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrl: './registrar.component.css'
 })
 export class RegistrarComponent {
+
+  registrarUsuario(f: NgForm) {
+    if (f.valid) {
+      const usuario = {
+        email: f.value.email,
+        password: f.value.password
+      };
+      console.log('Usuario registrado:', usuario);
+    }
+
+  }
 
 }
